b7e73de8782eb65fc9f4ab91a71f87ee2be6b280
sidgk <36684754+sidgk@users.noreply.github.com>
2020-08-30 12:20:05 +0200

    need to improve the code yet

--- app/repo.py
+++ app/repo.py
@@ -1,7 +1,5 @@
 import requests
 import sys
-import json
-from urllib.request import urlopen
 
 GITHUB_ENDPOINT = 'https://api.github.com'
 
@@ -27,12 +25,13 @@ print(f'[TRACE] username={username}, repository={repo_name}')
 		}
 	}
 '''
+
 '''
  Following headers are required to be added
  * Authorization: token <PERSONAL ACCESS TOKEN>
 '''
 request_headers = {
-	'Authorization': 'token dbd132a671e9a6783d867a2a53dca5ad1379f5e8'
+	'Authorization': 'token 3c8b33cb5ba16e9dccd1a9499c2361148889a759'
 }
 def getRepositoriesOfUser(username):
 	request = requests.get(GITHUB_ENDPOINT+'/users/'+username+'/repos?per_page=1000')
@@ -45,50 +44,40 @@ def findCommits(owner, repo, start_date, end_date):
 	request_url = f'{GITHUB_ENDPOINT}/repos/{owner}/{repo}/commits'
 	commits_in_repo = requests.get(request_url + request_params)
 	print("[TRACE] user commits url:", commits_in_repo.url)
-	#data = commits_in_repo.json()
-	#result = json.dumps(data, indent=2)
 	return commits_in_repo.json()
 
 def findAuthors(history_list):
-	#history_list = json.loads(history_list)
 	print('[DEBUG] Total Commits Found', len(history_list))
 	unique_authors = set()
 	for record in history_list:
-		print(record)
+        print("Siddu")
 		unique_authors.add(record["commit"]["author"]["name"])
 		print("[TRACE] Unique Commit URL:", record["url"])
 	print('[TRACE] Total unique authors found:', len(unique_authors))
 	return unique_authors
 
-def findFilesInReop(history_list):
-	print('[DEBUG] Total Commits Found', len(history_list))
-	unique_files = set()
-	for fil in history_list:
-		with urlopen(fil["url"]) as response:
-			source = response.read()
-
-		data = json.loads(source)
-		unique_files.add(data["files"][0]["filename"])
-		print("[TRACE] Unique Committed files :", data["files"][0]["filename"])
-	print('[TRACE] Total unique files found:', len(unique_files))
-	return unique_files
- 
 '''
 	The rest of the code starts from here
 '''
 repos = getRepositoriesOfUser(username)
-start_date = '2019-08-10T00:00:44+0000'
-end_date = '2020-08-29T00:00:44+0000'
+start_date = '2020-03-25T00:00:44+0000'
+end_date = '2020-07-25T00:00:44+0000'
 
 repo_commits = findCommits(
 			username, 
 			repo_name, 
 			start_date, 
 			end_date)
-#print(repo_commits)
 authors = findAuthors(repo_commits)
-filesnames = findFilesInReop(repo_commits)
-
 print("Unique authors:", authors)
-print("Unique files:", filesnames)
 
+def findFilesInReop(history_list):
+	print('[DEBUG] Total Commits Found', len(history_list))
+	unique_files = set()
+	for fil in history_list:
+		unique_files.add(fil["files"])
+		print("[TRACE] Unique Commit URL:", fil["files"])
+
+	print('[TRACE] Total unique files found:', len(unique_files))
+	return unique_files
+ 
